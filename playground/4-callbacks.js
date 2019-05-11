f55d505ee7a2276dbe5490a51e1555f0717fa5c3
syamala <syamaladevithiru@gmail.com>
2019-05-10 22:59:24 -0700

    callback-challenge

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,22 +1,31 @@
-setTimeout(()=>{
-    console.log('Two seconds are up.')
-},2000)
+//setTimeout(()=>{
+//    console.log('Two seconds are up.')
+//},2000)
 
-const names=['name1','name2','name3333']
+//const names=['name1','name2','name3333']
 
-const shortNames=names.filter((name)=>{
-    return name.length<=4
-})
+//const shortNames=names.filter((name)=>{
+//    return name.length<=4
+//})
 
-const geocode=(address,callback)=>{
+//const geocode=(address,callback)=>{
+//   setTimeout(()=>{
+//       const data={
+//           latitude:0,
+//           longitude:0
+//       }
+//       callback(data)
+//   },2000)
+//}
+//const data=geocode('philiphs',(data)=>{
+//   console.log(data)
+//})
+const add=(a,b,callback) =>{
     setTimeout(()=>{
-        const data={
-            latitude:0,
-            longitude:0
-        }
-        callback(data)
+        callback(a+b)
     },2000)
 }
-const data=geocode('philiphs',(data)=>{
-    console.log(data)
+
+add(1,2,(sum)=>{
+    console.log(sum)
 })
