79ebdcf433152af09b4691dc3cddbffde31b148d
syamala <syamaladevithiru@gmail.com>
2019-05-10 22:44:38 -0700

    using callback

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -7,3 +7,16 @@ const names=['name1','name2','name3333']
 const shortNames=names.filter((name)=>{
     return name.length<=4
 })
+
+const geocode=(address,callback)=>{
+    setTimeout(()=>{
+        const data={
+            latitude:0,
+            longitude:0
+        }
+        callback(data)
+    },2000)
+}
+const data=geocode('philiphs',(data)=>{
+    console.log(data)
+})
