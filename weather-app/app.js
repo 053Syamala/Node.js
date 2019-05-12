00e88d9df6924e0be88d0b01c05cb6a035fa2b96
syamala <syamaladevithiru@gmail.com>
2019-05-11 23:32:33 -0700

    adding geocode to another file

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request=require('request')
+const geocode=require('./utils/geocode')
 
 //const url='https://api.darksky.net/forecast/cee3cd9ce79f94fe1ce0d9b5af437237/37.8267,-122.4233?units=us'
 
@@ -19,25 +20,7 @@ const request=require('request')
 //    console.log(latitude,longitude)
 //})
 
-const geocode=(address,callback)=>{
-    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=pk.eyJ1Ijoic3lhbWFsYWRldmkiLCJhIjoiY2p2aHRsejBzMDZvNjRhdGV6aDF4eXp2ZiJ9.YpSy0_LEuA5LXM7jQiUwxA'
-    request({url:url,json:true},(error,response)=>{
-        if(error){
-            callback('unable to connect to the services',undefined)
-        }
-        else if(response.body.features.length===0){
-            callback('unable to find location.Try another search.',undefined)
-        }
-        else{
-            callback(undefined,{
-                latitude:response.body.features[0].center[0],
-                longitude:response.body.features[0].center[1],
-                location:response.body.features[0].place_name
-            })
-        }
-    })
-}
-geocode('Philadelphia',(error,data)=>{
+geocode('Boston',(error,data)=>{
     console.log('Error',error)
     console.log('Data',data)
 })
