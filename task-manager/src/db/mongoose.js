89f22c8cad91ffd7a854fb0b8d88112cb1a4cc5c
syamala <syamaladevithiru@gmail.com>
2019-05-19 14:49:36 -0700

    validate the values

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -8,16 +8,40 @@ const User=mongoose.model('User',{
         type:String
     },
     age:{
-        type:Number
+        type:Number,
+        validate(value){
+            if(value<10){
+                throw new Error('age must be a positive number')
+            }
+        }
     }
 })
 
 const me=new User({
-    name:'syamala',
-    age:20
+    name:'devi',
+    age:-10
 })
 me.save().then(()=>{
     console.log(me)
 }).catch((error)=>{
     console.log("Error!!",error)
 })
+/*const Task=mongoose.model('Task',{
+    description:{
+        type:String
+    },
+    completed:{
+        type:Boolean
+    }
+})
+
+const task=new Task({
+    description:'Learn mongoose',
+    completed:true
+})
+
+task.save().then(()=>{
+    console.log(task)
+}).catch((error)=>{
+    console.log(error)
+})*/
